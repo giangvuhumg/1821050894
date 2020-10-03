0331c5f74a8664a35a17dd09aed809a3650fda83
giangvuhumg <71376544+giangvuhumg@users.noreply.github.com>
2020-10-03 14:08:11 +0700

    Bổ sung nội dung web

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",function(){
+    $(function() {
+        $('nav ul li a').click(function() {
+            // Lấy section để hiển thị
+            var $section = $('#' + $(this).data('section'));
+            // Nếu đang hiện thì không làm gì.
+            // Nếu không, ẩn tất cả các se cho hiện (kiểu fade in) phần mong muốn.
+            if (!$section.is(':visible')) {
+                $('.hideable-section').hide();
+                $section.fadeIn();
+            }
+        });
+    });
+});
